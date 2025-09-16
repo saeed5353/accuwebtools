c3c11bff2a290338729a8b4e56538b92119ed907
saeed <meetsaeedbaig@gmail.com>
2025-09-17 00:23:44 +0500

    Fixes

--- blog-detail/index.php
+++ blog-detail/index.php
@@ -4,7 +4,7 @@
   include("../BlogPosts.php");
   
   // Get the slug from the URL
-  $slug = isset($_GET['slug']) ? $_GET['slug'] : '';
+  $slug = isset($_GET['id']) ? $_GET['id'] : '';
   
   if(empty($slug)) {
     // Redirect to homepage if no slug provided
